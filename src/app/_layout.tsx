250295823e547dc27bf642c244c724b8692ddfe0
Fauzan <heavenzownz@gmail.com>
2025-05-13 11:06:37 +0800

    init expo project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const _layout = () => {
+  return <Stack></Stack>;
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
